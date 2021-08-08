df4b23058f1b48e8272acf5647261f20b85d8861
Alexey Kosmachev <al.96@mail.ru>
2021-08-08 20:29:48 +0300

    fix

--- deploy-tank.sh
+++ deploy-tank.sh
@@ -12,5 +12,5 @@ yc compute instance create \
 --ssh-key $KEY_PATH \
 --create-boot-disk image-folder-id=standard-images,image-family=yandextank
 
-TANK_IP=`yc compute instance list --format json | jq -r '.[] | select(.name=="yandex-tank-$TANK_NAME") | .network_interfaces[0].primary_v4_address.one_to_one_nat.address'`
+TANK_IP=`yc compute instance list --format json | jq -r ".[] | select(.name==\"yandex-tank-$TANK_NAME\") | .network_interfaces[0].primary_v4_address.one_to_one_nat.address"`
 echo $TANK_IP > tank_ip.txt
